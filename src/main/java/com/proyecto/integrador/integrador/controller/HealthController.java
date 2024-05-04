938cc19ed3d04cfc198c0542b8db205127055d53
VictorCiceia <victormciceia@gmail.com>
2024-05-04 19:24:52 -0400

    Configuracion de Swagger

--- src/main/java/com/proyecto/integrador/integrador/controller/HealthController.java
+++ src/main/java/com/proyecto/integrador/integrador/controller/HealthController.java
@@ -1,13 +1,24 @@
 package com.proyecto.integrador.integrador.controller;
 
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.media.Content;
+import io.swagger.v3.oas.annotations.media.Schema;
+import io.swagger.v3.oas.annotations.responses.ApiResponse;
+import io.swagger.v3.oas.annotations.tags.Tag;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
+@RequestMapping("/health")
+@Tag(name = "Saludos", description = "Endpoint para validar el funcionamiento de la API")
 public class HealthController {
 
-    @RequestMapping("/health")
-    public String checkAPI(){
+    @GetMapping
+    @Operation(summary = "Validar API", description = "Obtener un mensaje de confirmacion de la API", responses = {
+            @ApiResponse(responseCode = "200", description = "Operación exitosa", content = @Content(schema = @Schema(implementation = String.class)))}
+    )
+    public String checkAPI() {
         return "<h1>The API is working ok!</h1>";
     }
 
