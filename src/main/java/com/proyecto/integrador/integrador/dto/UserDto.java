5f61d675c3113f8f3bf5b4fec8de245b5aca12bf
VictorCiceia <victormciceia@gmail.com>
2024-05-04 20:46:24 -0400

    Documentacion del objeto Usario

--- src/main/java/com/proyecto/integrador/integrador/dto/UserDto.java
+++ src/main/java/com/proyecto/integrador/integrador/dto/UserDto.java
@@ -1,5 +1,6 @@
 package com.proyecto.integrador.integrador.dto;
 
+import io.swagger.v3.oas.annotations.media.Schema;
 import lombok.AllArgsConstructor;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
@@ -11,16 +12,37 @@ import java.util.Date;
 @Getter
 @NoArgsConstructor
 @AllArgsConstructor
+@Schema(description = "Objeto que representa a un Usuario")
 public class UserDto {
 
+    @Schema(
+            description = "Identificador unico",
+            type = "string",
+            example = "1")
     private long id;
 
+    @Schema(
+            description = "Nombre",
+            type = "string",
+            example = "Juan")
     private String name;
 
+    @Schema(
+            description = "Apellido",
+            type = "string",
+            example = "Perez")
     private String lastName;
 
+    @Schema(
+            description = "Email",
+            type = "string",
+            example = "usuario@email.com")
     private String email;
 
+    @Schema(
+            description = "Fecha de creacion",
+            type = "Date",
+            example = "2024-05-04T23:51:14.243Z")
     private Date createdAt;
 
 }
